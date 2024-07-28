197c3eb633d810038dac4a77efad2dcb5685dacc
samu <146225785+SamyBravy@users.noreply.github.com>
2024-07-28 23:04:30 +0200

    Manca solo il .gitignore

--- cmake-build-debug/test/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/samyb/Documents/Altro/Primo anno università/Laboratorio di programmazione/IniManager/test/lib/googletest/googlemock
-# Build directory: C:/Users/samyb/Documents/Altro/Primo anno università/Laboratorio di programmazione/IniManager/cmake-build-debug/test/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/test/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/samyb/Documents/Altro/Primo anno università/Laboratorio di programmazione/IniManager/test/lib/googletest/googletest
-# Build directory: C:/Users/samyb/Documents/Altro/Primo anno università/Laboratorio di programmazione/IniManager/cmake-build-debug/test/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
